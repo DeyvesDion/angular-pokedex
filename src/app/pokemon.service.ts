05d367a85d67f12d24b3a0ccfc48f42fbaddcaaa
DeyvesDion <deyvesdion@gmail.com>
2022-05-13 14:24:18 +0200

    service

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { POKEMONS } from './pokemon/mock-pokemon-list';
+import { Pokemon } from './pokemon/pokemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
 
-  constructor() { }
+  getPokemonById(pokemonId: number): Pokemon|undefined {
+    return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+  }
+
+  getPokemonTypeList(): string[]{
+    return [
+      "Plante",
+      "Feu",
+      "Eau",
+      "Insect",
+      "Normal",
+      "Electrik",
+      "Poison",
+      "Fée",
+      "Vol",
+      "Combat",
+      "Psy"];
+  }
 }
--- src/app/pokemon/liste-pokemon/liste-pokemon.component.ts
+++ src/app/pokemon/liste-pokemon/liste-pokemon.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { POKEMONS } from '../mock-pokemon-list';
+import { PokemonService } from 'src/app/pokemon.service';
 import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-liste-pokemon',
   templateUrl: './liste-pokemon.component.html', 
 })
-export class ListePokemonComponent {
-  pokemonList: Pokemon[] = POKEMONS;
+export class ListePokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+  private pokemonService: PokemonService
+  ) { }
+  ngOnInit(){
+    this.pokemonList =this.pokemonService.getPokemonList();
+  }
   
   goToPokemon(pokemon: Pokemon) {
     this.router.navigate(['/pokemon', pokemon.id]);
