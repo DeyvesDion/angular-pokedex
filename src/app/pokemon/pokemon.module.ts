87cf2822a5a4c1448a9b4c6c3e6015f2a706481b
DeyvesDion <deyvesdion@gmail.com>
2022-05-13 19:44:43 +0200

    edit-form

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -5,9 +5,14 @@ import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.componen
 import { DorderCardDirective } from './dorder-card.directive';
 import { PokemoTypeColorPipe } from './pokemo-type-color.pipe';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
+import { FormsModule } from '@angular/forms';
+import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 
 const pokemonRoutes: Routes = [
+  { path: "edit/pokemon/:id", component: EditPokemonComponent },
   { path: "pokemons", component: ListePokemonComponent },
   { path: "pokemon/:id", component: DetailPokemonComponent }
 ];
@@ -17,11 +22,15 @@ const pokemonRoutes: Routes = [
     ListePokemonComponent,
     DetailPokemonComponent,
     DorderCardDirective,
-    PokemoTypeColorPipe
+    PokemoTypeColorPipe,
+    PokemonFormComponent,
+    EditPokemonComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(pokemonRoutes)
-  ]
+  ],
+  providers: [PokemonService]
 })
 export class PokemonModule { }
